9d41429419194506760b7bc863ec6900e214f2e4
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-12 20:49:30 -0800

    03 feature modeling script imports

--- scripts/03_feature_modeling.py
+++ scripts/03_feature_modeling.py
@@ -1,3 +1,9 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import os
+
 def feature_modeling(input_path, output_folder):
     # Load preprocessed data
     data = pd.read_csv(input_path)
